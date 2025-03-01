4d38ca3ddc8135f48c722c084539a6642df11505
thiagohc20 <thiago-henrique40@hotmail.com>
2025-03-01 20:07:36 -0300

    feat: adicionando servicos

---
+++ src/app/landingpage/components/landingpage-services/landingpage-services.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-landingpage-services',
+  imports: [ButtonModule],
+  templateUrl: './landingpage-services.component.html',
+  styleUrl: './landingpage-services.component.css',
+})
+export class LandingpageServicesComponent {}
--- src/app/landingpage/landingpage.component.ts
+++ src/app/landingpage/landingpage.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { LandingpageHeaderComponent } from './components/landingpage-header/landingpage-header.component';
 import { LandingpageFooterComponent } from './components/landingpage-footer/landingpage-footer.component';
 import { LandingpageHeroComponent } from './components/landingpage-hero/landingpage-hero.component';
+import { LandingpageServicesComponent } from './components/landingpage-services/landingpage-services.component';
 
 @Component({
   selector: 'app-landingpage',
@@ -10,6 +11,7 @@ import { LandingpageHeroComponent } from './components/landingpage-hero/landingp
     LandingpageHeaderComponent,
     LandingpageFooterComponent,
     LandingpageHeroComponent,
+    LandingpageServicesComponent,
   ],
   templateUrl: './landingpage.component.html',
   styleUrl: './landingpage.component.css',
