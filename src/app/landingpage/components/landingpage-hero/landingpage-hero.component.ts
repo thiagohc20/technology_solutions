27abf22fee42cd7eb523a6ec0f02340d24858031
thiagohc20 <thiago-henrique40@hotmail.com>
2025-03-01 19:33:38 -0300

    feat: adicionando hero

---
+++ src/app/landingpage/components/landingpage-hero/landingpage-hero.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-landingpage-hero',
+  imports: [ButtonModule],
+  templateUrl: './landingpage-hero.component.html',
+  styleUrl: './landingpage-hero.component.css',
+})
+export class LandingpageHeroComponent {}
--- src/app/landingpage/landingpage.component.ts
+++ src/app/landingpage/landingpage.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { LandingpageHeaderComponent } from './components/landingpage-header/landingpage-header.component';
 import { LandingpageFooterComponent } from './components/landingpage-footer/landingpage-footer.component';
+import { LandingpageHeroComponent } from './components/landingpage-hero/landingpage-hero.component';
 
 @Component({
   selector: 'app-landingpage',
   standalone: true,
-  imports: [LandingpageHeaderComponent, LandingpageFooterComponent],
+  imports: [
+    LandingpageHeaderComponent,
+    LandingpageFooterComponent,
+    LandingpageHeroComponent,
+  ],
   templateUrl: './landingpage.component.html',
   styleUrl: './landingpage.component.css',
 })
