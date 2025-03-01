2f7a037768f10b5845f2e4a97b0a6731f7dcf91b
thiagohc20 <thiago-henrique40@hotmail.com>
2025-03-01 16:26:52 -0300

    feat: instalando tailwind

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { LandingpageComponent } from './landingpage/landingpage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'landing_page',
+    pathMatch: 'full',
+  },
+  {
+    path: 'landing_page',
+    component: LandingpageComponent,
+  },
+  {
+    path: 'login',
+    component: LandingpageComponent,
+  },
+];
