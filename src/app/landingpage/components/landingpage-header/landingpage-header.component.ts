1c5c73754af2f080fd98df1cb4cd8da88a18af0c
thiagohc20 <thiago-henrique40@hotmail.com>
2025-03-01 18:15:34 -0300

    feat: adicionando header

---
+++ src/app/landingpage/components/landingpage-header/landingpage-header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landingpage-header',
+  imports: [],
+  templateUrl: './landingpage-header.component.html',
+  styleUrl: './landingpage-header.component.css'
+})
+export class LandingpageHeaderComponent {
+
+}
--- src/app/landingpage/landingpage.component.ts
+++ src/app/landingpage/landingpage.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { LandingpageHeaderComponent } from './components/landingpage-header/landingpage-header.component';
 
 @Component({
   selector: 'app-landingpage',
   standalone: true,
-  imports: [],
+  imports: [LandingpageHeaderComponent],
   templateUrl: './landingpage.component.html',
   styleUrl: './landingpage.component.css',
 })
