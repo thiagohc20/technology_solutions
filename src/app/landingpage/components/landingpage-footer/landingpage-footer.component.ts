5ad5a57b833977f999b2c0c3727a09b1da398657
thiagohc20 <thiago-henrique40@hotmail.com>
2025-03-01 18:47:04 -0300

    feat: adicionando footer landing-page

---
+++ src/app/landingpage/components/landingpage-footer/landingpage-footer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landingpage-footer',
+  imports: [],
+  templateUrl: './landingpage-footer.component.html',
+  styleUrl: './landingpage-footer.component.css'
+})
+export class LandingpageFooterComponent {
+
+}
--- src/app/landingpage/landingpage.component.ts
+++ src/app/landingpage/landingpage.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { LandingpageHeaderComponent } from './components/landingpage-header/landingpage-header.component';
+import { LandingpageFooterComponent } from './components/landingpage-footer/landingpage-footer.component';
 
 @Component({
   selector: 'app-landingpage',
   standalone: true,
-  imports: [LandingpageHeaderComponent],
+  imports: [LandingpageHeaderComponent, LandingpageFooterComponent],
   templateUrl: './landingpage.component.html',
   styleUrl: './landingpage.component.css',
 })
